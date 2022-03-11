64e04201075dd6736e0f9aab19b97208cbaa48cc
Matthew Toomey <mtoomeydev@gmail.com>
2022-03-11 00:00:51 -0500

    begin work on substring_match script

---
+++ substring_match/substring_match.rb
@@ -0,0 +1,6 @@
+puts "Substring Match"
+
+# should allow for user to enter an array of substrings, otherwise use default
+default_dict = ["the", "or", "thing", "hill", "car", "house", "tomato", "the", "i"]
+
+print "Enter an array of strings, or leave blank to use a default dictionary: "
