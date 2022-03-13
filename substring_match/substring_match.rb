0c42f041b25cfdcb12a103222d591b145f60e4a0
Matthew Toomey <mtoomeydev@gmail.com>
2022-03-13 13:16:31 -0400

    implement basic string-match function, single phrase matching

--- substring_match/substring_match.rb
+++ substring_match/substring_match.rb
@@ -1,13 +1,30 @@
 puts "Substring Match"
 
-# should allow for user to enter an array of substrings, otherwise use default
-default_dict = ["the", "or", "thing", "hill", "car", "house", "tomato", "the", "i"]
 
-print "Enter an array of strings, or leave blank to use a default dictionary: "
-dict = gets.chomp.split(' ')
-unless dict == []
-  puts "Using dictionary #{dict}"
-else
-  dict = default_dict
-  puts "Using default dictionary #{dict}"
+
+
+def get_dictionary
+  # allow for user to enter an array of substrings, otherwise use default_dict values
+  default_dict = ["the", "or", "thing", "hill", "car", "house", "tomato", "the", "i"]
+  print "Enter an array of strings, or leave blank to use a default dictionary: "
+  dict = gets.chomp.split(' ') # split into array on each space between words
+  unless dict == []
+    puts "Using dictionary #{dict}"
+  else
+    dict = default_dict
+    puts "Using default dictionary #{dict}"
+  end
+  dict
 end
+
+def substrings(match_phrase, dictionary)
+  # get the number of matches for the given phrase in the dictionary
+  dictionary.reduce(Hash.new(0)) do |result, string|
+    if string.downcase == match_phrase.downcase
+      result[match_phrase] += 1
+    end
+      result
+  end
+end
+
+puts substrings("the", get_dictionary)
