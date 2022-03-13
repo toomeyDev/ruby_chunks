987b496bb1f8ecb03ade3f16920f598b6c9a10c1
Matthew Toomey <mtoomeydev@gmail.com>
2022-03-13 12:59:11 -0400

    allow for user-input dictionaries

--- substring_match/substring_match.rb
+++ substring_match/substring_match.rb
@@ -4,3 +4,10 @@ puts "Substring Match"
 default_dict = ["the", "or", "thing", "hill", "car", "house", "tomato", "the", "i"]
 
 print "Enter an array of strings, or leave blank to use a default dictionary: "
+dict = gets.chomp.split(' ')
+unless dict == []
+  puts "Using dictionary #{dict}"
+else
+  dict = default_dict
+  puts "Using default dictionary #{dict}"
+end
