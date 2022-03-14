5b7e9d55a464a3fa1ec9fdd0f6c5691479e9de1a
Matthew Toomey <mtoomeydev@gmail.com>
2022-03-14 10:21:58 -0400

    finish stock_picker core functionality, support edge cases

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -5,18 +5,28 @@ def stock_picker(prices)
 	# Loop through prices and find highest profit for a given starting value
 	# compared to all other prices in the array
     for i in price_index...prices.size
-      profit = prices[i] - prices[price_index]
+      buy = prices[price_index]
+      sell = prices[i]
+
+      if buy <= sell
+        profit = sell - buy
+      elsif buy > sell
+        profit = sell - buy
+      end
+ 
       if profit > maximum_profit
 		maximum_profit = profit
 		best_buy = [price_index, i]
       end
+      
     end
+    
    end
-   p "Prices: #{prices}"
-   p "Profit: #{maximum_profit}"
-   best_buy
+   result = "Prices for today: #{prices}\n"
+   result += "Buy at #{prices[best_buy[0]]}$, sell at #{prices[best_buy[1]]}$ for a profit of #{maximum_profit}$\n\n"
+   result # return a formatted report giving the best buy/sell positions and profit
 end
 
-p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
-p stock_picker([1, 2, 3, 4, 5, 6, 7, 8])
-p stock_picker([500, 2, 4, 5, 6, 100, 10, 25, 0])
+puts stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+puts stock_picker([1, 2, 3, 4, 5, 6, 7, 8])
+puts stock_picker([500, 2, 4, 5, 6, 100, 10, 25, 0])
