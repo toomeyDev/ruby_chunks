bd91805d3fd682c5179cd3dee768a0158314e4d6
Matthew Toomey <mtoomeydev@gmail.com>
2022-03-22 13:47:08 -0400

    implement basic mastermind gameplay, works with predefined codes currently

--- mastermind/mastermind.rb
+++ mastermind/mastermind.rb
@@ -1,5 +1,7 @@
 require "./roles.rb"
 
+TURN_LIMIT = 12
+
 class Game
   def initialize
     @code_maker = Codemaker.new
@@ -12,7 +14,41 @@ class Game
     \nOne player is the codemaker, and will choose a hidden code made up of four colors, color choices include red, green, blue, yellow, black and white.
     \nThe other player is the codebreaker, and must guess the codebreaker's code in 12 turns.
     \b\b\b\bCodebreaker will receive feedback after each guess to assist in cracking the code."
+    @code_maker.create_code('red','green','red','blue')
+    TURN_LIMIT.times do |turn|
+      puts "Turn number: #{turn+1}"
+      break if code_guess()
+    end
   end
+
+  def code_guess
+    colors = @code_breaker.get_guess(@code_maker)
+    correct_positions = []
+    correct_colors = []
+
+    colors.each_with_index do |color, index|
+      if color == @code_maker.hidden_colors[index]
+        correct_positions << "*" # indicate a correct guess
+      else
+        correct_positions << "-" # indicate an incorrect guess
+      end
+      
+      if @code_maker.hidden_colors.include? color
+        correct_colors << color
+      end
+
+    end
+    puts correct_positions.join
+    puts "Correct colors: #{correct_colors.join}"
+
+    # check for victory condition
+    if correct_positions.join == "****"
+      puts "Cracked the code, great job!"
+      return true
+    end
+    return false
+  end
+
 end
 
 mastermind_game = Game.new
--- mastermind/roles.rb
+++ mastermind/roles.rb
@@ -3,6 +3,14 @@ class Codemaker
   def initialize
     @hidden_colors = [] # store chosen colors for the code which must be solved
   end
+
+  def create_code(color_a, color_b, color_c, color_d)
+    hidden_colors.append(color_a)
+    hidden_colors.append(color_b)
+    hidden_colors.append(color_c)
+    hidden_colors.append(color_d)
+  end
+
 end
 
 class Codebreaker
@@ -10,4 +18,12 @@ class Codebreaker
   def initialize
     @colors = []
   end
+
+  def get_guess(codemaker)
+    print "Make your guess in the format c1/c2/c3/c4, ie: red/green/blue/yellow\nGuess: "
+    guess = gets.chomp
+    guess = guess.split("/")
+    return guess
+  end
+
 end
