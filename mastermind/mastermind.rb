935bceebc86364f2272b97b7cb070b0105d81834
Matthew Toomey <mtoomeydev@gmail.com>
2022-03-22 14:24:16 -0400

    implement CPU control for codebreaker role, allow for both roles to be switched to player or computer control

--- mastermind/mastermind.rb
+++ mastermind/mastermind.rb
@@ -4,8 +4,8 @@ TURN_LIMIT = 12
 
 class Game
   def initialize
-    @code_maker = Codemaker.new
-    @code_breaker = Codebreaker.new
+    @code_maker = Codemaker.new(true)
+    @code_breaker = Codebreaker.new(false)
   end
 
   def intro
@@ -14,10 +14,23 @@ class Game
     \nOne player is the codemaker, and will choose a hidden code made up of four colors, color choices include red, green, blue, yellow, black and white.
     \nThe other player is the codebreaker, and must guess the codebreaker's code in 12 turns.
     \b\b\b\bCodebreaker will receive feedback after each guess to assist in cracking the code."
+    victory = false
+    turns = 0
     @code_maker.create_code
     TURN_LIMIT.times do |turn|
       puts "Turn number: #{turn+1}"
-      break if code_guess()
+      victory = code_guess()
+      turns = turn
+      break if victory # break out if victory condition is detected
+    end
+    end_message(turns, victory)
+  end
+
+  def end_message(turns, victory_condition)
+    if victory_condition
+      puts "Congratulations, cracked the code in #{turns} turns!"
+    else
+      puts "Too bad, codebreaker couldn't crack the code in 12 turns!"
     end
   end
 
@@ -28,21 +41,21 @@ class Game
 
     colors.each_with_index do |color, index|
       if color == @code_maker.hidden_colors[index]
-        correct_positions << "*" # indicate a correct guess
+        correct_positions << "x" # indicate a correct guess
       else
-        correct_positions << "-" # indicate an incorrect guess
+        correct_positions << "o" # indicate an incorrect guess
       end
       
       if @code_maker.hidden_colors.include? color
-        correct_colors << color
+        correct_colors << color if !correct_colors.include? color
       end
 
     end
     puts correct_positions.join
-    puts "Correct colors: #{correct_colors.join}"
+    puts "Correct colors: #{correct_colors.join(" ")}"
 
     # check for victory condition
-    if correct_positions.join == "****"
+    if correct_positions.join == "xxxx"
       puts "Cracked the code, great job!"
       return true
     end
--- mastermind/roles.rb
+++ mastermind/roles.rb
@@ -2,8 +2,8 @@ COLORS = ['red','blue','green','yellow','black','white']
 
 class Codemaker
   attr_accessor :hidden_colors
-  def initialize
-    @human = false # keep track of whether codemaker is CPU or player
+  def initialize(human=false)
+    @human = human # keep track of whether codemaker is CPU or player
     @hidden_colors = [] # store chosen colors for the code which must be solved
   end
 
@@ -14,24 +14,47 @@ class Codemaker
         hidden_colors.append(color)
       end
     else
-      puts 'human'
+      code = get_code
+      code.each {|color| hidden_colors.append(color)}
+      puts "Codemaker has chosen a hidden code!"  
     end
+  end
+
+  def get_code
+    print "Select a code for the other player to crack using format c1/c2/c3/c4, ie: red/green/green/blue\nYour code: "
+    code = gets.chomp
+    code = code.split("/")
+    puts code
+    return code
   end
 
 end
 
 class Codebreaker
   attr_accessor :colors
-  def initialize
-    @human = true # keep track of whether codebreaker is CPU or player
-    @colors = []   
+  def initialize(human=true)
+    @human = human # keep track of whether codebreaker is CPU or player
+    @colors = [] 
+  end
+
+  def cpu_guess
+    guess = []
+    4.times do  
+      color = COLORS[rand(6)]
+      guess.append(color)
+    end
+    guess
   end
 
   def get_guess(codemaker)
-    print "Make your guess in the format c1/c2/c3/c4, ie: red/green/blue/yellow\nGuess: "
-    guess = gets.chomp
-    guess = guess.split("/")
-    return guess
+    unless @human
+      return cpu_guess
+    else
+      print "Make your guess in the format c1/c2/c3/c4, ie: red/green/blue/yellow\nGuess: "
+      guess = gets.chomp
+      guess = guess.split("/")
+      return guess
+    end
   end
 
 end
