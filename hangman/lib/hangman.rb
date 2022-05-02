61987893702d34ab08fffcb5092aded93e458ee1
Matthew Toomey <mtoomeydev@gmail.com>
2022-05-02 14:58:30 -0400

    implement core functionality for guess-checking in hangman script

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -29,19 +29,40 @@ class Game
         puts "Selecting random word.."
         hidden_word = random_word()
         p hidden_word
-        player_guess
+        # keep track of any characters guessed by the player
+        guessed_characters = []
+        5.times do
+            guessed_characters.push(player_guess)
+            check_word_against_guesses(hidden_word, guessed_characters)
+        end
     end
 
-    def player_guess
+    # prompt the player for a guess to check against the hidden word
+    def player_guess()
         puts "Choose a letter for your guess: "
         print "Guess: "
         gets.chomp
     end
 
+    # check the currently selected word against all previously
+    # guessed letters entered by the player
+    def check_word_against_guesses(word, guesses)
+        p guesses
+        word_characters = word.split('')
+        # for each letter guessed, check against the hidden word
+        word_characters.each do |character|
+            if guesses.include? character
+                print character
+            else
+                print '_'
+            end
+        end
+    end
+
     # select a random word from the dictionary dataset
     def random_word()
         word_choice = nil
-        File.open('hangman/english-10000-no-swears.txt', 'r') do |words|
+        File.open('english-10000-no-swears.txt', 'r') do |words|
             word_index = Random.rand(words.readlines().size)
             words.seek 0
             while word_index > 0
